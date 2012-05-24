3933c1123874474d8eaecd140c9f4232fa93bcd2
guilhermeanselmo2@gmail.com <guilhermeanselmo2@gmail.com@05a7af3a-75eb-202e-e3ac-9f6f340786e8>
2012-05-24 04:57:47 +0000

    Float

--- trunk/software-basico/assembler/Identifier.cpp
+++ trunk/software-basico/assembler/Identifier.cpp
@@ -345,3 +345,50 @@ pair<bool, vector<string> > identifyTypeJ(string instruction)
 	results.second = tokens;
 	return results;
 }
+
+pair<bool, vector<string> > identifyTypeFloat(string instruction)
+{
+	regex correct("(:?[a-zA-Z]+(?:[0-9]*[a-zA-Z]*)*: )?[a-z]+\\.[a-z] \\$(:?(:?.{2})|(:?zero)),\\$(:?(:?.{2})|(:?zero)),\\$(:?(:?.{2})|(:?zero))");
+	regex correctInstruction("[a-z]+\\.[a-z]");
+	regex correctParameter1("\\$(:?(:?zero)|(:?.{2}))");
+	regex correctParameter2("\\$(:?(:?zero)|(:?.{2}))");
+	regex correctParameter3("\\$(:?(:?zero)|(:?.{2}))");
+	regex label("[a-zA-Z]+(?:[0-9]*[a-zA-Z]*)*: ");
+	vector<regex> rules (4);
+	vector<string> tokens (4);
+	pair<bool, vector<string> > results;
+	results.second = tokens;
+	cmatch result;
+	int i = 0;
+	
+	rules[0] = correctInstruction;
+	rules[1] = correctParameter1;
+	rules[2] = correctParameter2;
+	rules[3] = correctParameter3;
+	
+	if(regex_match(instruction.begin(), instruction.end(), correct))
+	{
+		if (regex_search(instruction.c_str(), result, label))
+		{
+			instruction = instruction.substr (result.position() + result.length(), instruction.length() - (result.position() + result.length()));
+		}
+		results.first = 1;
+		while(i < 4)
+		{
+			regex_search(instruction.c_str(), result, rules[i]);
+			tokens[i] = instruction.substr (result.position(),result.length());
+			//LOG(LEVEL_INFO) << "token = " << tokens[i];
+			instruction = instruction.substr (result.position() + result.length(), instruction.length() - (result.position() + result.length()));
+			//LOG(LEVEL_INFO) << "instruction" << instruction;
+			i++;
+		}
+		//LOG(LEVEL_INFO) << "It is correct, R2";
+		LOG(LEVEL_INFO) << "Float tokens " << tokens[0] << ", " << tokens[1] << ", " << tokens[2] << ", " << tokens[3];
+		results.second = tokens;
+		return results;
+	}
+	//LOG(LEVEL_ERROR) << "Error sintatic error R2";
+	results.first = 0;
+	results.second = tokens;
+	return results;
+}
